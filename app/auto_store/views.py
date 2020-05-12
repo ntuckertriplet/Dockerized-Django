964593d156eea347e45cb4516e6f01fbba78baed
njtucker <njtucker@iastate.edu>
2020-05-12 14:38:19 -0500

    wrong application..

--- app/auto_store/views.py
+++ app/auto_store/views.py
@@ -11,3 +11,65 @@ from .forms import EmployeeCreationForm
 
 def index_view(request):
     return render(request, 'index.html')
+
+
+def products(request):
+    return render(request, 'products.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+
+@login_required(login_url='/')
+def employees(request):
+    return render(request, 'employee.html')
+
+
+def customer_memo(request):
+    return render(request, 'customerMemo.html')
+
+
+def employee_holiday(request):
+    return render(request, 'employeeHoliday.html')
+
+
+def shutdown(request):
+    return render(request, 'shutdown.html')
+
+
+def w2(request):
+    return render(request, 'w2.html')
+
+
+def ceo_message(request):
+    return render(request, 'ceoMessage.html')
+
+
+def covid19(request):
+    return render(request, 'covid19.html')
+
+
+def testing(request):
+    return render(request, 'testing.html')
+
+
+def bulletin(request):
+    return render(request, 'bulletin.html')
+
+
+@login_required(login_url='/')
+def add_user(request):
+    if request.method == 'POST':
+        f = EmployeeCreationForm(request.POST, initial={})
+        if f.is_valid():
+            f.save()
+            return redirect('employees')
+
+    else:
+        f = EmployeeCreationForm()
+        return render(request, 'adduser.html', {'form': f})
+
+
+def help(request):
+    return render(request, 'help.html')
--- app/invulnerable_app/urls.py
+++ app/invulnerable_app/urls.py
@@ -10,6 +10,18 @@ from auto_store import views
 urlpatterns = [
     path('', views.index_view, name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('products/', views.products, name='products'),
+    path('services/', views.services, name='services'),
+    path('employees/', views.employees, name='employees'),
+    path('customerMemo/', views.customer_memo, name='customer_memo'),
+    path('employeeHoliday/', views.employee_holiday, name='employee_holiday'),
+    path('shutdown/', views.shutdown, name='shutdown'),
+    path('w2/', views.w2, name='w2'),
+    path('ceoMessage/', views.ceo_message, name='ceomessage'),
+    path('covid19/', views.covid19, name='covid19'),
+    path('testing/', views.testing, name='testing'),
+    path('bulletin/', views.bulletin, name='bulletin'),
+    path('addUser/', views.add_user, name='add_user'),
     path("admin/", admin.site.urls),
 ]
 
